ce91cf1152ff6b65312bb0728254fc716cb0b87c
drkchiloll <samwomack_@me.com>
2016-02-03 06:09:14 -0600

    convert all function literals to =>

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const BrowserWindow = electron.BrowserWindow;
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
@@ -15,7 +15,7 @@ app.on('window-all-closed', function() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 1280, height: 600});
 
@@ -26,7 +26,7 @@ app.on('ready', function() {
   mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
