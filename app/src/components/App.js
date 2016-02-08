ed637110757ec2f4132da1901e023c70d0516870
drkchiloll <samwomack_@me.com>
2016-02-08 09:35:45 -0600

    Very basic structure the React App (top-level) component

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Login from './Login';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      creds: {},
+      loggedIn: false
+    };
+  }
+  login() {
+    // Not sure Yet
+    // Probably something along the lines of GET AgentStats
+  }
   render() {
     return (
-      <div> Cisco UCCX Builk Provisionings </div>
+      <div>
+        <Login login={this.login} loggedIn={this.state.loggedIn}/>
+        {/* Other Stuff Here */}
+      </div>
     );
   }
 }
