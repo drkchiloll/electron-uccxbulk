905a20e92ef678748e155aa8f45c8a986d22f945
drkchiloll <samwomack_@me.com>
2016-02-08 14:40:29 -0600

    wrote basic handleLogin method

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -11,7 +11,12 @@ export default class Login extends React.Component {
     this.handleLogin = this.handleLogin.bind(this);
   }
   handleLogin() {
-    console.log(this.refs.ip.value);
+    let [ip, user, pass] =
+      [this.refs.ip.value, this.refs.user.value, this.refs.pass.value];
+    loginEvt.emit('login', {ip: ip, user: user, pass: pass});
+    loggedIn.on('loggedIn', (skills) => {
+      console.log(skills);
+    })
   }
   render() {
     var displayLogin = this.dispLogin();
@@ -20,26 +25,38 @@ export default class Login extends React.Component {
     };
     return (
       <div style={displayLogin} className='panel panel-primary'>
+        <h4 className='col-sm-offset-1'> UCCX Login </h4>
         <div
           style={{margin: '20px 20px 20px 20px'}}
           className='form-horizontal'>
-          <h4> UCCX Login </h4>
           <div className='form-group'>
             <label className='col-sm-2 control-label'> Server: </label>
             <div className='col-sm-10'>
-              <input ref='ip' type='text' className='form-control' placeholder='Server IP'/>
+              <input
+                ref='ip'
+                type='text'
+                className='form-control'
+                placeholder='Server IP'/>
             </div>
           </div>
           <div className='form-group'>
             <label className='col-sm-2 control-label'> UserId: </label>
             <div className='col-sm-10'>
-              <input type='text' className='form-control' placeholder='User Id'/>
+              <input
+                ref='user'
+                type='text'
+                className='form-control'
+                placeholder='User Id'/>
             </div>
           </div>
           <div className='form-group'>
             <label className='col-sm-2 control-label'> Password: </label>
             <div className='col-sm-10'>
-              <input type='password' className='form-control' placeholder='Password'/>
+              <input
+                ref='pass'
+                type='password'
+                className='form-control'
+                placeholder='Password'/>
             </div>
           </div>
           <div className='form-group'>
