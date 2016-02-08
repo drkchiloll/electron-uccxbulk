9cb2510d671e9c7fe417c8bbfc0c73cbd2d69e10
drkchiloll <samwomack_@me.com>
2016-02-08 16:37:36 -0600

    finalize login method in top-level comp.

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -6,18 +6,34 @@ export default class App extends React.Component {
     super();
     this.state = {
       creds: {},
+      message: null,
       loggedIn: false
     };
+    this.login = this.login.bind(this);
   }
   login() {
-    // Not sure Yet
-    // Probably something along the lines of GET AgentStats
+    loggedIn.on('loggedIn', (stats) => {
+      this.setState({loggedIn: true});
+    });
+    loggedIn.on('loginError', (status) => {
+      this.setState({message: status});
+    })
   }
   render() {
     return (
       <div>
-        <Login login={this.login} loggedIn={this.state.loggedIn}/>
+        <Login
+          login={this.login}
+          loggedIn={this.state.loggedIn}
+          message={this.state.message}/>
         {/* Other Stuff Here */}
+        <div
+          style={{display: this.state.loggedIn ? 'block': 'none'}}
+          className='panel panel-primary'>
+          <div style={{margin: '20px 20px 20px 20px'}}>
+            New Stuff Here
+          </div>
+        </div>
       </div>
     );
   }
