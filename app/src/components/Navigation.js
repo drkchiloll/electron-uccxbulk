e0e06866cfbe8fa79fad7d274f684083bb81dec7
drkchiloll <samwomack_@me.com>
2016-02-29 06:45:23 -0600

    Changed to Nav Pills (buttons) and changed to Stacked (vertical) Moved Logout Icon to the top of the Nav Stack Added an offset to the column (probably could just use a container too)

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -4,26 +4,28 @@ export default class Navigation extends React.Component {
   render() {
     var activeTab = this.props.activeTab;
     return (
-      <ul className='nav nav-tabs'>
-        <li role='presentation'
-            className={activeTab==='skills' ? 'active' : ''}>
-          <a id='skills'
-             onClick={this._setTab.bind(this)}>Skills</a>
-        </li>
-        <li role='presentation'
-            className={activeTab==='resources' ? 'active' : ''}>
-          <a id='resources'
-             onClick={this._setTab.bind(this)}>Resources</a>
-        </li>
-        <li role='presentation'
-            className={activeTab==='logout' ? 'active' : ''}>
-          <a id='logout'
-             onClick={this.props.onLogout}>
-             <span className='glyphicon glyphicon-log-out'
-                   style={{color: 'red', fontSize: '1.2em'}}></span>
-          </a>
-        </li>
-      </ul>
+      <div className='col-sm-offset-1 col-sm-2'>
+        <ul className='nav nav-pills nav-stacked'>
+          <li role='presentation'
+              className={activeTab==='logout' ? 'active' : ''}>
+            <a id='logout'
+               onClick={this.props.onLogout}>
+               <span className='glyphicon glyphicon-log-out'
+                     style={{color: 'red', fontSize: '1.2em'}}></span>
+            </a>
+          </li>
+          <li role='presentation'
+              className={activeTab==='skills' ? 'active' : ''}>
+            <a id='skills'
+               onClick={this._setTab.bind(this)}>Skills</a>
+          </li>
+          <li role='presentation'
+              className={activeTab==='resources' ? 'active' : ''}>
+            <a id='resources'
+               onClick={this._setTab.bind(this)}>Resources</a>
+          </li>
+        </ul>
+      </div>
     );
   }
   _setTab(e) {
