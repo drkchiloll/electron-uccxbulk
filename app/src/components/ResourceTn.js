0e509d57a54f8bdaecd6ccb51981994a4fe875fe
drkchiloll <samwomack_@me.com>
2016-03-03 17:29:11 -0600

    refactoring and adding in additional parameters passed from Resources Component (not a very good commit as a lot of changes/additions occurred)

--- app/src/components/ResourceTn.js
+++ app/src/components/ResourceTn.js
@@ -31,7 +31,21 @@ export default class ResourceTn extends React.Component {
     }
   }
   _agentDetails(agent) {
-    var actAgent = this.props.agentSelectSkill;
+    var actAgent = this.props.selectedAgents;
+    var agentId = agent.userID;
+    var agntSkill, skillsToAdd, skill;
+    if(agent.skillMap.skillCompetency) {
+      skill = agent.skillMap.skillCompetency;
+      if(agent.skillsToAdd) {
+        skillsToAdd = agent.skillsToAdd;
+      }
+      agntSkill = this._renderAgentSkillTable(agentId, skill, skillsToAdd);
+    } else {
+      if(agent.skillsToAdd) {
+        skillsToAdd = agent.skillsToAdd;
+      }
+      agntSkill = this._renderAgentSkillTable(agentId, null, skillsToAdd);
+    }
     return (
       <div style={{
             display: (!agent.expand) ? 'none' : 'block',
@@ -40,21 +54,64 @@ export default class ResourceTn extends React.Component {
            className="thumbnail">
         <div className="caption">
           <p>{agent.firstName} {agent.lastName}</p>
-          <hr/>
-          <ul className='nav nav-pills'>
-            <li role='presentation'>
-              <button id={agent.userID}
-                      onClick={this._setActive.bind(this)}
-                      className={`btn btn-xs btn-${actAgent===agent.userID ? 'primary' : 'link'}`}>
-                Skills Map
-              </button>
-            </li>
-          </ul>
+          <button id={agent.userID}
+                  onClick={this._setActive.bind(this)}
+                  className={`btn btn-${actAgent===agent.userID ? 'primary btn-sm' : 'link btn-md'}`}>
+            Skills Map
+          </button>
+          {agntSkill}
         </div>
       </div>
     );
   }
   _setActive(e) {
     this.props.setAgentSkill(e.target.id);
+  }
+  _renderAgentSkillTable(id, agntSkill, skillsToAdd) {
+    var genList = [1,2,3,4,5,6,7,8,9,10];
+    var len;
+    if(!agntSkill) len = 0;
+    else len = agntSkill.length + 10;
+    return (
+      <table className={`${id} table`}>
+        <thead>
+          <tr><th>SkillName</th><th>Skill Value</th></tr>
+        </thead>
+        <tbody>
+          {agntSkill ? this._agntSkills(agntSkill) : undefined}
+          {skillsToAdd
+            ? this._agntSkillsToAdd(skillsToAdd, len, genList)
+            : <tr></tr>
+          }
+        </tbody>
+      </table>
+    );
+  }
+  _agntSkills(agntSkill) {
+    return agntSkill.map((skill, i) => {
+      var skillname = skill.skillNameUriPair['@name'];
+      var comp = skill.competencelevel;
+      return (
+        <tr key={i}>
+          <td>{skillname==='placeholder' ? undefined : skillname}</td>
+          <td>{comp}</td>
+        </tr>
+      );
+    });
+  }
+  _agntSkillsToAdd(skills, len, genList) {
+    return skills.map((skill, i) => {
+      return (
+        <tr key={len + i}>
+          <td style={{color:'red'}}>{skill}</td>
+          <td>
+            <select>{genList.map((num, i) => {
+              return <option key={i}>{num}</option>;
+              })}
+            </select>
+          </td>
+        </tr>
+      )
+    });
   }
 }
